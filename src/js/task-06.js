28753c04f5ca2ba39998f4c941fbce2b7ef81293
Alena Shykova <shining8smaragd@gmail.com>
2022-09-29 16:57:30 +0300

    valid
    task 6

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,29 +1,13 @@
-/*const inputElements = document.querySelector('#validation-input');
-const textLenght = inputElements.getAttribute('data-length');
-console.log(textLenght);
-
-function onInputBlur(event) {
-    if (event.currentTarget.value.length === Number(textLenght)) {
-        inputElements.classList.remove("invalid");
-        inputElements.classList.add("valid");
-    }
-    else { inputElements.classList.add("invalid") };
-}
-
-inputElements.addEventListener('blur', onInputBlur);*/
-
-const input = document.querySelector( '#validation-input' )
-
-input.addEventListener( 'blur', onValidBlur )
+const input = document.querySelector('#validation-input');
 
 
-function onValidBlur () {
-    if ( input.value.length === 6 ) {
-        input.classList.add( 'valid' )
-        input.classList.remove( 'invalid' )
-    }
-    else {
-        input.classList.remove( 'valid' )
-        input.classList.add('invalid')
-    }
-}
+input.addEventListener('blur', () => {
+    if (input.value.length !== Number(input.dataset.length)) {
+        input.classList.remove('valid');
+        input.classList.add('invalid');
+
+    } else {
+        input.classList.remove('invalid');
+        input.classList.add('valid');
+    };
+});
